9d063a512a2d767b77c5fe06f2c6a5f68f19ce4f
Hamlet-Fansion <fanpeilin123@126.com>
2021-03-25 13:39:11 +0800

    try commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Hamlet on 2021/3/25.
+//
+
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    QPushButton button("Hello world!", nullptr);
+    button.resize(200, 100);
+    button.show();
+    return QApplication::exec();
+}
